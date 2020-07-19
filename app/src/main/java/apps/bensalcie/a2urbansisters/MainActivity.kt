cd0a61699fcee73769f18279677e318778c9213b
Benard Ngoda <bensalcie@gmail.com>
2020-07-19 15:14:19 +0300

    Update MainActivity.kt
    ....

--- app/src/main/java/apps/bensalcie/a2urbansisters/MainActivity.kt
+++ app/src/main/java/apps/bensalcie/a2urbansisters/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findNavController(R.id.nav_host_reusable_fragment)
+       
     }
 }
